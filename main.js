b72803f158d232e851129efc07f3e5caf9e6a024
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-11 05:52:54 +0500

    simple

---
+++ main.js
@@ -0,0 +1,92 @@
+// Boolean 1
+
+// let A = 2;
+
+// let res = A > 0;
+//  console.log(res);
+
+// Boolean 2
+
+// let a = 3;
+// let b = -3
+
+// let res = a >=0; && b < -2;
+
+// console.log(res);
+
+
+// Boolean 3
+
+// let a = 3;
+// let b = 2;
+// let c = 1;
+
+// let res = a > b > c;
+
+// console.log(res);
+
+
+// Boolean 4
+
+// let a = 3;
+// let b = 5;
+
+// let res = (a % 2 = 1  && b % 2 =1)
+
+// console.log(res);
+
+// Boolean 5
+
+// let a = 3;
+// let b = 4;
+
+// let res = (a % 2 = 1  && b % 2 =1 || a % 2 =0 && b % 2 = 0 )
+
+// console.log(res);
+
+// Boolean 6
+
+// let a = 3;
+// let b = 4;
+
+// let res = (a % 2 = 1  && b % 2 =0 || a % 2 =0 && b % 2 = 1)
+
+// console.log(res);
+
+// Boolean 7
+
+// let a = 3;
+// let b = 4;
+// let c = 4;
+
+// let res = (a % 2 = 0  && b % 2 =0  c % 2 = 0)
+
+// console.log(res);
+
+// Boolean 8
+
+// let a = 3;
+// let b = 4;
+// let c = 4;
+
+// let res = (a % 2 = 0  && b % 2 =1 && c % 2 = 1 ||  a % 2 = 1  && b % 2 =0 && c % 2 = 1  ||  a % 2 = 1  && b % 2 =1 && c % 2 = 0)
+
+// console.log(res);
+
+// Boolean 9
+
+// let a = 3;
+// let b = 4;
+// let c = 4;
+
+// let res = (a % 2 = 0  && b % 2 =0 && c % 2 = 1 ||  a % 2 = 0  && b % 2 =1 && c % 2 = 0  ||  a % 2 = 1  && b % 2 =0 && c % 2 = 0)
+
+// console.log(res);
+
+
+// Boolean 10;
+
+// let a = 32;
+
+// let res = a % 2 = 0;
+// console.log(res);
